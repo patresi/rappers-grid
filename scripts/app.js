36e50eca19ca8c73d5f6984a0540c5c25b8c6094
bertolo1988 <tiagobertolo@gmail.com>
2017-03-04 19:53:47 +0000

    improved css; added more rappers and sorted them by name

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    function masonryDraw() {
+    function drawGrid() {
         imagesLoaded(document.querySelector('.grid'), function () {
             console.log('all images are loaded');
             let wall = new Freewall('.grid');
@@ -26,20 +26,66 @@ document.addEventListener("DOMContentLoaded", function () {
         el: '#app',
         data: {
             rappersList: [
-                { name: 'Kodak Black', isFree: false, pic: 'https://s-media-cache-ak0.pinimg.com/originals/27/0a/03/270a039f3e086bbb348a6271b9906148.jpg' },
-                { name: 'Young Thug', isFree: true, pic: 'http://blameebro.com/wp-content/uploads/2016/09/Young-Thug_04-04-2016.jpg' },
-                { name: 'Gucci Mane', isFree: true, pic: 'http://thehollywoodunlocked.com/wp-content/uploads/2016/05/1540776442.jpg' },
-                { name: 'Kanye West', isFree: true, pic: 'https://www.biography.com/.image/c_fill,cs_srgb,dpr_1.0,g_face,h_300,q_80,w_300/MTE1ODA0OTcxNDc4MTg5NTgx/kanye-west-362922-1-402.jpg' },
-                { name: 'Migos', isFree: true, pic: 'http://static.stereogum.com/uploads/2015/06/Migos-640x347.jpg' },
-                { name: 'XXXTENTACION', isFree: true, pic: 'https://pbs.twimg.com/media/C4Bg0LmWIAEGXhT.jpg' },
-                { name: 'Chief Keef', isFree: true, pic: 'http://www.billboard.com/files/styles/article_main_image/public/media/chief-keef-650-430-a.jpg' },
-                { name: 'Drake', isFree: true, pic: 'http://www.rap-up.com/app/uploads/2015/10/drake-art.jpg' },
-                { name: 'Kendrick Lamar', isFree: true, pic: 'http://ontheblacklist.net/wp-content/uploads/2016/08/kendrick-lamar-featuring-lady-gaga-partynauseous.jpg' }
+                {
+                    "name": "Chief Keef",
+                    "isFree": true,
+                    "pic": "http://www.billboard.com/files/styles/article_main_image/public/media/chief-keef-650-430-a.jpg"
+                },
+                {
+                    "name": "Drake",
+                    "isFree": true,
+                    "pic": "http://www.rap-up.com/app/uploads/2015/10/drake-art.jpg"
+                },
+                {
+                    "name": "Gucci Mane",
+                    "isFree": true,
+                    "pic": "http://www.rap-up.com/app/uploads/2016/07/gucci-ice-cream.jpg"
+                },
+                {
+                    "name": "Kanye West",
+                    "isFree": true,
+                    "pic": "https://www.biography.com/.image/c_fill,cs_srgb,dpr_1.0,g_face,h_300,q_80,w_300/MTE1ODA0OTcxNDc4MTg5NTgx/kanye-west-362922-1-402.jpg"
+                },
+                {
+                    "name": "Kendrick Lamar",
+                    "isFree": true,
+                    "pic": "http://cache.umusic.com/_sites/kendricklamar.com/images/og.jpg"
+                },
+                {
+                    "name": "Kevin Gates",
+                    "isFree": false,
+                    "pic": "https://s-media-cache-ak0.pinimg.com/originals/1c/b2/7d/1cb27dd7e734b2403f704870f3e12a0f.png"
+                },
+                {
+                    "name": "Kodak Black",
+                    "isFree": false,
+                    "pic": "https://s-media-cache-ak0.pinimg.com/originals/27/0a/03/270a039f3e086bbb348a6271b9906148.jpg"
+                },
+                {
+                    "name": "Migos",
+                    "isFree": true,
+                    "pic": "http://static.stereogum.com/uploads/2015/06/Migos-640x347.jpg"
+                },
+                {
+                    "name": "Playboi Carti",
+                    "isFree": true,
+                    "pic": "https://ifudontknowblog.files.wordpress.com/2017/01/cartiiiiii.jpg"
+                },
+                {
+                    "name": "XXXTENTACION",
+                    "isFree": true,
+                    "pic": "https://i1.sndcdn.com/avatars-000283540284-d1h3wr-t500x500.jpg"
+                },
+                {
+                    "name": "Young Thug",
+                    "isFree": true,
+                    "pic": "http://blameebro.com/wp-content/uploads/2016/09/Young-Thug_04-04-2016.jpg"
+                }
             ]
         }
     })
     Vue.nextTick(function () {
-        masonryDraw();
+        drawGrid();
     })
 });
 
