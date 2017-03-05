2071d94cd7ee586eb94de5d3b564d2a5350f4434
bertolo1988 <tiagobertolo@gmail.com>
2017-03-05 00:52:34 +0000

    compress and commit images
    also hide whole grid until everything is setup

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
+
+    function showGrid() {
+        $('.grid').show();
+    }
+
     function drawGrid() {
-        imagesLoaded(document.querySelector('.grid'), function () {
-            console.log('all images are loaded');
+        imagesLoaded($('.grid'), () => {
+            showGrid();
             let wall = new Freewall('.grid');
             wall.reset({
                 selector: '.rapper-thumb',
@@ -18,89 +23,22 @@ document.addEventListener("DOMContentLoaded", function () {
             wall.fitWidth();
         });
     }
+
     Vue.component('rapper-thumbnail', {
         props: ['rapper'],
         template: '<div :id="rapper.name" class="rapper-thumb"><div class="pic-container"><img class="bars" src="images/cell-bars.png-c200" ><img :src="rapper.pic"></div><div class="name-container">{{ rapper.name }}</div></div>'
     })
+
     var app = new Vue({
         el: '#app',
         data: {
-            rappersList: [
-                {
-                    "name": "Chief Keef",
-                    "isFree": true,
-                    "pic": "http://www.billboard.com/files/styles/article_main_image/public/media/chief-keef-650-430-a.jpg"
-                },
-                {
-                    "name": "Drake",
-                    "isFree": true,
-                    "pic": "http://www.rap-up.com/app/uploads/2015/10/drake-art.jpg"
-                },
-                {
-                    "name": "Future",
-                    "isFree": true,
-                    "pic": "http://www.billboard.com/files/media/future-portrait-june-2015-billboard-650.jpg"
-                },
-                {
-                    "name": "Gucci Mane",
-                    "isFree": true,
-                    "pic": "http://www.rap-up.com/app/uploads/2016/07/gucci-ice-cream.jpg"
-                },
-                {
-                    "name": "Kanye West",
-                    "isFree": true,
-                    "pic": "https://www.biography.com/.image/c_fill,cs_srgb,dpr_1.0,g_face,h_300,q_80,w_300/MTE1ODA0OTcxNDc4MTg5NTgx/kanye-west-362922-1-402.jpg"
-                },
-                {
-                    "name": "Kendrick Lamar",
-                    "isFree": true,
-                    "pic": "http://cache.umusic.com/_sites/kendricklamar.com/images/og.jpg"
-                },
-                {
-                    "name": "Kevin Gates",
-                    "isFree": false,
-                    "pic": "https://s-media-cache-ak0.pinimg.com/originals/1c/b2/7d/1cb27dd7e734b2403f704870f3e12a0f.png"
-                },
-                {
-                    "name": "Kodak Black",
-                    "isFree": false,
-                    "pic": "https://s-media-cache-ak0.pinimg.com/originals/27/0a/03/270a039f3e086bbb348a6271b9906148.jpg"
-                },
-                {
-                    "name": "Lil Uzi Vert",
-                    "isFree": true,
-                    "pic": "https://uproxx.files.wordpress.com/2016/05/lil-uzi-vert.jpg?quality=100&w=650"
-                },
-                {
-                    "name": "Lil Wayne",
-                    "isFree": true,
-                    "pic": "https://www.biography.com/.image/c_fill,cs_srgb,dpr_1.0,g_face,h_300,q_80,w_300/MTE5NTU2MzE2MDgwOTk3ODk5/lil-wayne-369134-1-402.jpg"
-                },
-                {
-                    "name": "Migos",
-                    "isFree": true,
-                    "pic": "http://static.stereogum.com/uploads/2015/06/Migos-640x347.jpg"
-                },
-                {
-                    "name": "Playboi Carti",
-                    "isFree": true,
-                    "pic": "https://ifudontknowblog.files.wordpress.com/2017/01/cartiiiiii.jpg"
-                },
-                {
-                    "name": "XXXTENTACION",
-                    "isFree": true,
-                    "pic": "https://i1.sndcdn.com/avatars-000283540284-d1h3wr-t500x500.jpg"
-                },
-                {
-                    "name": "Young Thug",
-                    "isFree": true,
-                    "pic": "http://blameebro.com/wp-content/uploads/2016/09/Young-Thug_04-04-2016.jpg"
-                }
-            ]
+            rappersList
         }
     })
-    Vue.nextTick(function () {
+
+    Vue.nextTick(() => {
         drawGrid();
     })
+
 });
 
---
+++ scripts/rappers-list.js
@@ -0,0 +1,77 @@
+var rappersList = [
+    {
+        "name": "Bobby Shmurda",
+        "isFree": false,
+        "pic": "images/rappers/bobby-shmurda.jpg"
+    },
+    {
+        "name": "Chief Keef",
+        "isFree": true,
+        "pic": "images/rappers/chief-keef.jpg"
+    },
+    {
+        "name": "Drake",
+        "isFree": true,
+        "pic": "images/rappers/drake.jpg"
+    },
+    {
+        "name": "Future",
+        "isFree": true,
+        "pic": "images/rappers/future.jpg"
+    },
+    {
+        "name": "Gucci Mane",
+        "isFree": true,
+        "pic": "images/rappers/gucci-mane.jpg"
+    },
+    {
+        "name": "Kanye West",
+        "isFree": true,
+        "pic": "images/rappers/kanye-west.jpg"
+    },
+    {
+        "name": "Kendrick Lamar",
+        "isFree": true,
+        "pic": "images/rappers/kendrick-lamar.jpg"
+    },
+    {
+        "name": "Kevin Gates",
+        "isFree": false,
+        "pic": "images/rappers/kevin-gates.jpg"
+    },
+    {
+        "name": "Kodak Black",
+        "isFree": false,
+        "pic": "images/rappers/kodak-black.jpg"
+    },
+    {
+        "name": "Lil Uzi Vert",
+        "isFree": true,
+        "pic": "images/rappers/lil-uzi-vert.jpg"
+    },
+    {
+        "name": "Lil Wayne",
+        "isFree": true,
+        "pic": "images/rappers/lil-wayne.jpg"
+    },
+    {
+        "name": "Migos",
+        "isFree": true,
+        "pic": "images/rappers/migos.jpg"
+    },
+    {
+        "name": "Playboi Carti",
+        "isFree": true,
+        "pic": "images/rappers/playboi-carti.jpg"
+    },
+    {
+        "name": "XXXTENTACION",
+        "isFree": true,
+        "pic": "images/rappers/xxxtentacion.jpg"
+    },
+    {
+        "name": "Young Thug",
+        "isFree": true,
+        "pic": "images/rappers/young-thug.jpg"
+    }
+];
